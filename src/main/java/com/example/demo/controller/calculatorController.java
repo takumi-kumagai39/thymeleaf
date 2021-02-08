f3ae88539d9912096dade0eb61f57496da992a71
takumi-kumagai39 <tm.momo86@outlook.jp>
2021-02-08 22:49:05 +0900

    add new file

--- src/main/java/com/example/demo/controller/calculatorController.java
+++ src/main/java/com/example/demo/controller/calculatorController.java
@@ -1,11 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.model.Calculator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/calculator")
 public class calculatorController {
-    
+
+    @RequestMapping("/index")
+    public String index(Model model){
+        model.addAttribute("calculator",new Calculator());
+        return "sample";
+    }
+
+    @RequestMapping(value = "/result" , method = RequestMethod.POST)
+    public String calculatorSubmit(@ModelAttribute Calculator calculator , Model model) {
+        long sum = calculator.getNumber1() + calculator.getNumber2();
+
+        model.addAttribute("calculator", sum);
+        return "result";
+    }
 }
---
+++ src/main/java/com/example/demo/controller/model/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller.model;
+
+public class Calculator {
+
+    private long number1;
+    private long number2;
+
+    public long getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(long number1) {
+        this.number1 = number1;
+    }
+
+    public long getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(long number2) {
+        this.number2 = number2;
+    }
+}
